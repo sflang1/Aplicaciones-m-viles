5d1225995d596b611d0b40cacef5ef61b4a94205
Sebastián Felipe <slandinezg@gmail.com>
2015-04-18 09:38:05 -0500

    Primer commit del Repo

---
+++ Android/ViewsSeleccionables/app/src/main/java/com/example/sebastinfelipe/viewsseleccionables/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.sebastinfelipe.viewsseleccionables;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
+
+    ListView lista;
+    String[] colores;
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lista=(ListView) findViewById(R.id.list);
+        lista.setOnItemClickListener(this);
+        colores=getResources().getStringArray(R.array.colores);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+    {
+        String msg=getResources().getString(R.string.msg);
+        msg=msg+" "+colores[position];
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+    }
+}
